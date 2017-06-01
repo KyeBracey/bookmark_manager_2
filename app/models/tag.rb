ce99d5b6d900bee177086a0f994b8b1a65ca49d9
Kynosaur <kye.bracey@gmail.com>
2017-06-01 15:50:47 +0100

    Implement functionality for user to visit any /tags/ page and display links with that tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,9 +5,11 @@ require 'dm-postgres-adapter'
 class Tag
 
   include DataMapper::Resource
-  
+
   property :id,    Serial
   property :name, String
+
+  has n, :links, :through => Resource
 
 
 end
